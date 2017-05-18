c5ecbb9c74aa9e5160558db2f2b0f5f73880e655
Darnell <Darnell@DESKTOP-BCPGTJ2>
2017-05-18 15:41:31 +0800

    添加测试用例

--- Split_words/src/test/java/test/TestCase.java
+++ Split_words/src/test/java/test/TestCase.java
@@ -9,9 +9,15 @@ import cn.clvstudio.webcrawling.CommentMyapp;
 import cn.clvstudio.webcrawling.GetComment;
 
 public class TestCase {
+	/**
+	 * 单次爬取应用宝评论数据
+	 */
 	@Test
 	public void test1(){
-		Map<String, Object> commentMap = GetComment.getCommentMap("com.tencent.mobileqq","676","1","0.8325424489262343","");
+		String apkName = "com.tencent.mobileqq";
+		String apkCode = "676";
+		String fresh = "0.8325424489262343";
+		Map<String, Object> commentMap = GetComment.getCommentMap(apkName,apkCode,1,fresh,"");
 		boolean success = (Boolean) commentMap.get("success");
 		if(success){
 			String contextData = (String) commentMap.get("contextData");
@@ -24,6 +30,38 @@ public class TestCase {
 			}
 		}else{
 			System.out.println(false );
+		}
+	}
+	/**
+	 * 多次自动爬取应用宝评论数据测试
+	 */
+//	@Test
+	public void test2(){
+		String apkName = "com.tencent.mobileqq";
+		String apkCode = "676";
+		String fresh = "0.8325424489262343";
+		int num = 3;
+		int p = 1;
+		String contextData = "";
+		for(int i = 0 ;i<num;i++){
+			Map<String, Object> commentMap = GetComment.getCommentMap(apkName,apkCode,p,fresh,contextData);
+			boolean success = (Boolean) commentMap.get("success");
+			if(success){
+				contextData = (String) commentMap.get("contextData");
+				int hasNext = (Integer) commentMap.get("hasNext");
+				List<CommentMyapp> commentList = (List<CommentMyapp>) commentMap.get("commentList");
+				System.out.println("第"+i+"次 ；contextData : "+contextData+" ; hasNext : " + hasNext);
+				for(int j=0;j<commentList.size();j++){
+					CommentMyapp comment = commentList.get(j);
+					System.out.println(comment);
+				}
+				
+				p++;
+				
+			}else{
+				System.out.println(false +" : "+ i);
+				break;
+			}
 		}
 	}
 }
